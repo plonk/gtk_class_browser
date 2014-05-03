6e7f468b43b246edd4db84173e684ca99d1396ee
Yoteichi <plonk@piano.email.ne.jp>
2014-05-03 12:05:12 +0900

    extract method

--- object_list.rb
+++ object_list.rb
@@ -8,41 +8,54 @@ class ObjectList < Gtk::ScrolledWindow
   attr_reader :selected
 
   # 上矢印が昇順がいい。
-  SORT_ASCENDING = Gtk::SORT_DESCENDING
-  SORT_DESCENDING = Gtk::SORT_ASCENDING
+  UP_ARROW = Gtk::SORT_DESCENDING
+  DOWN_ARROW = Gtk::SORT_ASCENDING
 
   def initialize headers, attr_list = [:to_s]
     super()
+
+    self.hscrollbar_policy = POLICY_AUTOMATIC
+
     @objects = []
     @attr_list = attr_list.map(&:to_proc)
-    self.hscrollbar_policy = POLICY_AUTOMATIC
     types = [String] * @attr_list.size
     @list_store = ListStore.new(*[Integer] + types)
     @treeview = create(TreeView, @list_store)
-    types.size.times do |i|
-      col = TreeViewColumn.new(headers[i], CellRendererText.new, text: i+1)
-      col.resizable = true
-      col.clickable = true
-      col.sort_indicator = false
-      col.signal_connect('clicked') do
-        @treeview.columns.each {|c| if c!=col then c.sort_indicator = false end }
-        if !col.sort_indicator? or col.sort_order == SORT_DESCENDING
-          col.sort_indicator = true
-          col.sort_order = SORT_ASCENDING
-          @list_store.set_sort_column_id(i+1, Gtk::SORT_ASCENDING)
-        else
-          col.sort_order = SORT_DESCENDING
-          @list_store.set_sort_column_id(i+1, Gtk::SORT_DESCENDING)
-        end
-      end
-      @treeview.append_column col
-    end
+
+    install_columns(headers)
     @treeview.search_column = 1
 
     @treeview.signal_connect('cursor-changed', &method(:on_cursor_changed))
     @treeview.signal_connect('row-activated', &method(:on_row_activated))
     add @treeview
   end
+
+  def install_columns headers
+    nfields = headers.size
+    nfields.times do |i|
+      create(TreeViewColumn, headers[i], CellRendererText.new, {text: i+1},
+             resizable: true, clickable: true) do |col|
+        col.signal_connect('clicked') do
+          @treeview.columns.each do |c|
+            if c!=col
+              c.sort_indicator = false
+            end
+          end
+
+          if !col.sort_indicator? or col.sort_order==DOWN_ARROW
+            col.sort_indicator = true
+            col.sort_order = UP_ARROW
+            @list_store.set_sort_column_id(i+1, Gtk::SORT_ASCENDING)
+          else
+            col.sort_order = DOWN_ARROW
+            @list_store.set_sort_column_id(i+1, Gtk::SORT_DESCENDING)
+          end
+        end
+        @treeview.append_column col
+      end
+    end
+  end
+
 
   def on_row_activated _, path, column
     changed
